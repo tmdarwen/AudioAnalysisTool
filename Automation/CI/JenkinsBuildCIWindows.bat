e471da1d49c30672c8ee986bc05ed58d55a19713
Terence Darwen <tmdarwen@gmail.com>
2017-11-10 13:27:26 -0600

    Fix to Jenkins build script for Windows [JIRA-ATT-7]

--- Automation/CI/JenkinsBuildCIWindows.bat
+++ Automation/CI/JenkinsBuildCIWindows.bat
@@ -1,6 +1,6 @@
 call "%VisualStudioPath%\vcvarsall.bat" amd64
-mkdir PhaseVocoderBuilt
-cd PhaseVocoderBuilt
+mkdir AudioAnalysisToolBuilt
+cd AudioAnalysisToolBuilt
 cmake -G "%CMakeGeneratorString%" -D CMAKE_PREFIX_PATH="%QtMSVCPath%" ../Source
-devenv PhaseVocoder.sln /Build Debug
-devenv PhaseVocoder.sln /Build Release
+devenv AudioAnalysisTool.sln /Build Debug
+devenv AudioAnalysisTool.sln /Build Release
