b960fead7e54f086ca3bae37786aa78cf9cc783d
Terence Darwen <tmdarwen@gmail.com>
2017-11-10 22:42:30 -0600

    Fix to the Jenkins build script for Windows [JIRA-ATT-7]

--- Automation/CI/JenkinsBuildCIWindows.bat
+++ Automation/CI/JenkinsBuildCIWindows.bat
@@ -1,6 +1,6 @@
-call "%%VisualStudioPath%%\vcvarsall.bat" amd64
+REM call "%%VisualStudioPath%%\vcvarsall.bat" amd64
 mkdir AudioAnalysisToolBuilt
 cd AudioAnalysisToolBuilt
-cmake -G "%%CMakeGeneratorString%%" -D CMAKE_PREFIX_PATH="%%QtMSVCPath%%" ../Source
+cmake -G "%CMakeGeneratorString%" -D CMAKE_PREFIX_PATH="%QtMSVCPath%" ../Source
 devenv AudioAnalysisTool.sln /Build Debug
 devenv AudioAnalysisTool.sln /Build Release
